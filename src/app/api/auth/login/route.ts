6e21cfc58c77c8d662ff5ceb202c4d880a50bbbd
shama <shama.dmitry@gmail.com>
2025-03-25 20:04:16 +0200

    add login API route

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,11 +1,59 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import User from "@/src/models/user.model";
+import { connectDB } from "@/src/lib/db";
+import { generateToken } from "@/src/lib/utils";
 
-export async function GET() {
+export async function POST(req: NextRequest) {
+  connectDB();
+
+  const body = await req.json();
+
+  const { email, password } = body;
+
+  if (!email || !password) {
+    return NextResponse.json(
+      { message: "All fields are required" },
+      {
+        status: 400,
+      }
+    );
+  }
+
   try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "User not found" },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return NextResponse.json(
+        { message: "Invalid credentials" },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    await generateToken(user._id.toString());
+
     return NextResponse.json({
-      message: "Successfully login",
+      _id: user._id,
+      email: user.email,
+      fullName: user.fullName,
+      profilePic: user.profilePic,
     });
   } catch (error) {
+    console.log("error", error);
+
     return NextResponse.json(
       { error: "Internal server error", message: error },
       { status: 500 }
--- src/app/api/auth/singup/route.ts
+++ src/app/api/auth/singup/route.ts
@@ -1,9 +1,21 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { email, fullName, password } = body;
+
   try {
+    if (!email || !fullName || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        {
+          status: 400,
+        }
+      );
+    }
+
     return NextResponse.json({
-      message: "Successfully signup",
+      message: "Successfully singup",
     });
   } catch (error) {
     return NextResponse.json(
