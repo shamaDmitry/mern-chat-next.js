2e2770cfc2118dd240e692a6bbe1f33c760d2197
shama <shama.dmitry@gmail.com>
2025-03-25 15:46:55 +0200

    base structure

---
+++ src/lib/db.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+export const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+  }
+};
